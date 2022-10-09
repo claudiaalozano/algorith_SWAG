aac67c2901c71cc7608d62f269d663d9d348bb30
lauralardies <lrodrlar@myuax.com>
2022-10-09 19:12:31 +0200

    función gráfica entrenamiento y pérdida

--- swag.py
+++ swag.py
@@ -1,13 +1,11 @@
-from ast import increment_lineno
-import tensorflow
 import timeit
-from keras.model import Model,Sequential
-from keras.layers import Input, Embedding, LSTM, LSTM, Dense, concatenate, Dropout, Flatten, Conv2D, MaxPool12D, Activation, MaxPooling2D
+from keras.model import Model, Sequential
+from keras.layers import Input, Embedding, LSTM, Dense, concatenate, Dropout, Flatten, Conv2D, MaxPool12D, Activation, MaxPooling2D
 from keras.utils.np_utils import to_categorical
 from keras.optimizers import RMSprop, Adam
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLRonPlateau
-from keras import backend as k
+from keras import backend as K
 from keras.utils.generic_utils import get_costum_objects
 from keras.datasets import cifar10
 from keras.utils import np_utils
@@ -27,3 +25,15 @@ from sklearn.moder_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import itertools
 
+numpy.random.seed(7)
+
+def plot_(history):
+    training_loss1 = history.history['loss']
+    test_loss1 = history.history['val_loss']
+    epoch_count = range(1, len(training_loss1) + 1)
+    plt.plot(epoch_count, training_loss1, 'r--')
+    plt.plot(epoch_count, test_loss1, 'b-')
+    plt.legend(['Training Loss', 'Test Loss'])
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.show();
